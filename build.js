746525b05378efcef002c349f1900910e561f87e
Subhajit Sahu <wolfram77@gmail.com>
2020-02-10 03:38:17 +0530

    v0.0.14

--- build.js
+++ build.js
@@ -2,12 +2,32 @@ const csv = require('csv');
 const path = require('path');
 const fs = require('fs');
 
-function read(file) {
-  var fpath = path.join('data', file), z = [];
+function read(file, fn) {
+  var fpath = path.join('data', file);
   if(!fs.existsSync(fpath)) return Promise.resolve();
   return new Promise((fres) => {
     var stream = fs.createReadStream(fpath).pipe(csv.parse({columns: true}));
-    stream.on('data', (row) => z.push(row));
-    stream.on('end', () => fres(z));
+    stream.on('data', fn);
+    stream.on('end', fres);
   });
 };
+
+function convert(row) {
+  var code = row['Code'];
+  var names = row['Name(s)'];
+  var color = (row['Colour']||'').replace('(', ', ').replace(')', '');
+  var type = row['Purpose']!=null? row['Purpose']:`color (${color})`;
+  var status = row['Status'].search(/Approved in (the EU|\d+)/)>=0? 'e ':'';
+  status += row['Status'].search(/Approved in the US/)>=0? 'u ':'';
+  status = status.trim();
+  return {code, names, type, status};
+};
+
+function load() {
+  var rdy = [], z = [];
+  for(var i=100; i<=1000; i++)
+    rdy.push(read(`e${i}.csv`, (row) => z.push(convert(row))));
+  return Promise.all(rdy);
+};
+
+load().then((rows) => fs.writeFileSync('data.json', JSON.stringify(rows)));
